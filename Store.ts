5fd090b37ea96d5d6d8062f3ca9abf82ab198404
Sakin790 <sakinislam79@gmail.com>
2025-01-22 05:02:00 -0500

    init

---
+++ Store.ts
@@ -0,0 +1,31 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(1)
+  const doubleCount = computed(() => count.value * 2)
+
+  const loading = ref(false)
+  const error: any = ref(null)
+  const data = ref(null)
+
+  function increment() {
+    count.value++
+  }
+
+  async function fetchData() {
+    loading.value = true
+    error.value = null
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos') // Replace with your API endpoint
+      data.value = response.data
+    } catch (err) {
+      error.value = 'Failed to fetch data'
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { count, doubleCount, increment, loading, error, data, fetchData }
+})
